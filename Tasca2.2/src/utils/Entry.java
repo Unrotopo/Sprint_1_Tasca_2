65c9997d8363d4206ea92133a760c8c46cb95c65
Luis Portas <otrorotopo@gmail.com>
2025-03-15 11:47:15 +0100

    finished product
    it works! i think

--- Tasca2.2/src/utils/Entry.java
+++ Tasca2.2/src/utils/Entry.java
@@ -5,60 +5,76 @@ import java.util.Scanner;
 
 public class Entry {
 
-    private static Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
 
     public static byte readByte(String message) throws InputMismatchException {
-        try {
+        System.out.println(message);
+        if (sc.hasNextByte()) {
             return sc.nextByte();
-        } catch (InputMismatchException e) {
-            throw new InputMismatchException(message);
+        } else {
+            throw new InputMismatchException();
         }
     }
 
     public static int readInt(String message) throws InputMismatchException {
-        try {
+        System.out.println(message);
+        if (sc.hasNextInt()) {
             return sc.nextInt();
-        } catch (InputMismatchException e) {
-            throw new InputMismatchException(message);
+        } else {
+            throw new InputMismatchException();
         }
     }
 
     public static float readFloat(String message) throws InputMismatchException {
-        try {
-            return sc.nextFloat();
-        } catch (InputMismatchException e) {
-            throw new InputMismatchException(message);
+        System.out.println(message);
+        String input = sc.next();
+        if (input.contains(".")) {
+            return Float.parseFloat(input);
+        } else {
+            throw new InputMismatchException();
         }
     }
 
     public static double readDouble(String message) throws InputMismatchException {
-        try {
-            return sc.nextDouble();
-        } catch (InputMismatchException e) {
-            throw new InputMismatchException(message);
+        System.out.println(message);
+        String input = sc.next();
+        if (input.contains(".")) {
+            return Double.parseDouble(input);
+        } else {
+            throw new InputMismatchException();
         }
     }
 
-    public static char readChar(String message) throws Exception {
-        try {
-            return sc.next().charAt(0);
-        } catch (Exception e) {
-            throw new Exception(message);
-        }
+    public static char readChar(String message) throws EntryException {
+        System.out.println(message);
+        String input = sc.next();
+        if (input.length() == 1) {
+            if (Character.isLetter(input.charAt(0))) {
+                return input.charAt(0);
+            } else {
+                throw new EntryException(message);
+            }
+        } throw new EntryException(message);
     }
 
-    public static String readString(String message) throws Exception {
-        try {
-            return sc.next();
-        } catch (Exception e) {
-            throw new Exception(message);
+    public static String readString(String message) throws EntryException {
+        System.out.println(message);
+        String input = sc.nextLine();
+        if (!input.isEmpty()) {
+            return input;
+        } else {
+            throw new EntryException(message);
         }
     }
-    public static boolean readBoolean(String message) throws Exception {
-        try {
-            return sc.nextBoolean();
-        } catch (Exception e) {
-            throw new Exception(message);
+    public static boolean readBoolean(String message) throws EntryException {
+        System.out.println(message);
+        String input = sc.nextLine();
+        if (input.equalsIgnoreCase("y")) {
+            return true;
+        } else if (input.equalsIgnoreCase("n")) {
+            return false;
+        } else {
+            throw new EntryException(message);
         }
     }
 }
